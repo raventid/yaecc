0a443d47b590f802362105b33ac5f7b539cd525d
raventid <juliankul@gmail.com>
2025-06-30 22:08:23 +0800

    compiler: exit lexer if --lex is passed

--- compiler/bin/main.ml
+++ compiler/bin/main.ml
@@ -110,6 +110,10 @@ let main () =
     let tokens = lex content in
     printf "Tokens:\n";
     print_tokens tokens;
+  end;
+  
+  if stage == Lex then begin 
+    printf "Lexer flag --lex is set, stopping after lexing\n";
     exit 0
   end;
   
